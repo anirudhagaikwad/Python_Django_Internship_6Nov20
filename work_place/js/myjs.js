d6c0aedbbab3f336491369b0d69a4b268d47d53b
unknown <gaikwad.anirudha@gmail.com>
2020-11-13 12:10:40 +0530

    commit 4

---
+++ work_place/js/myjs.js
@@ -0,0 +1,38 @@
+// its single line comment
+
+/* multiline Comment
+multi */
+
+//var ,let,const
+
+var x=50;
+var y=30;
+
+
+//function 
+
+function testmyfun()
+{
+    var x=10;
+    let y=20;
+    const z="abcd";
+    
+    console.log("the value of x : "+x)
+    
+    alert("the value of y = "+y)
+}
+
+
+
+
+function contstmt()
+{
+    if(x>y)
+    {
+        alert("X is greter than y")
+    }
+    else
+    {
+        alert("x is less than y")
+    }
+}
